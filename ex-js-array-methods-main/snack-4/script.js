871fc5fcef52d1a3115c4affb556c47901eeed43
monica <monica.poli91@libero.it>
2025-11-21 15:33:30 +0100

    snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -3,7 +3,10 @@ const posts = [
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(post => post.author);
+console.log(authors);
